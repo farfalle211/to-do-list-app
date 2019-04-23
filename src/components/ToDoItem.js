50b1d0cec98f14be0d2b3e382e1f18214afe29fb
farfalle211 <farfalle211@gmail.com>
2019-04-23 13:48:02 -0500

    edited state to check and uncheck boxes

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,9 +3,12 @@ import React from "react";
 function ToDoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.description} />
-      <p style={{color: props.item.description ? "black" : "red"}}>{props.item.text}</p>
-      <p>{props.item.description}</p>
+      <input 
+        type="checkbox" 
+        checked={props.item.completed} 
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p>{props.item.text}</p>
     </div>
   )
 }
